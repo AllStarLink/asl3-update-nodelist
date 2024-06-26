241f1409491fc65d85980512909d6682a7c0bd00
Jason D. McCormick <jason@mfamily.org>
2024-06-26 13:01:43 -0400

    Skip github archiving

--- builder/dockerbuild.sh
+++ builder/dockerbuild.sh
@@ -20,6 +20,21 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;;
+	-r)
+      APTLY_REPO="asl-$2"
+      shift
+      shift	
+      ;;
+	-u)
+      APTLY_USER="$2"
+      shift
+      shift
+      ;;
+    -g)
+      APTLY_GPG="$2"
+      shift
+      shift
+      ;;
 	-*|--*|*)
       echo "Unknown option $1"
 	  shift
@@ -89,3 +104,16 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install gh
 gh release upload -R AllStarLink/asl3-update-nodelist $GH_REL $ALL_PKG_ROOT/_debs/*.deb
 
 docker image rm --force $D_TAG
+
+find $GH_REL $ALL_PKG_ROOT/_debs/*.deb -name "*.deb" | \
+	xargs -I {} -d '\n' curl --fail --user ${APTLY_USER} \
+	-X POST -F 'file=@"{}"' \
+	 https://repo-admin.allstarlink.org/api/files/${APTLY_REPO}-{$OPERATING_SYSTEMS}
+
+curl --fail --user ${APTLY_USER} -X POST \
+	https://repo-admin.allstarlink.org/api/repos/${APTLY_REPO}/file/${APTLY_REPO}-${OPERATING_SYSTEMS}
+
+curl --fail --user ${APTLY_USER} -X PUT -H "content-Type: application/json" \
+	--data '{"Signing": {"Batch": true, "Passphrase": "${APTLY_GPG}"}}' \
+	"https://repo-admin.allstarlink.org/api/publish/:./${OPERATING_SYSTEMS}"
+
